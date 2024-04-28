b00619022ae84c039113943f073fd23fde65fdbb
satheesh kumar <4798470+dreamstudio-satheesh@users.noreply.github.com>
2024-04-28 19:52:54 +0530

    app

--- db.js
+++ db.js
@@ -25,6 +25,9 @@ function fetchAndStoreCategories(db) {
     fetch('https://app.satsweets.com/api/categories')
         .then(response => response.json())
         .then(categories => {
+            if (!Array.isArray(categories)) {
+                throw new TypeError('Expected an array of categories, received: ' + typeof categories);
+            }
             const transaction = db.transaction(['categories'], 'readwrite');
             const store = transaction.objectStore('categories');
             store.clear();
@@ -33,4 +36,19 @@ function fetchAndStoreCategories(db) {
             });
         })
         .catch(error => console.error('Error fetching categories:', error));
-};
+}
+
+
+function fetchAndStoreProducts(db) {
+    fetch('https://app.satsweets.com/api/products')  // Adjust the URL as necessary
+        .then(response => response.json())
+        .then(products => {
+            const transaction = db.transaction(['products'], 'readwrite');
+            const store = transaction.objectStore('products');
+            store.clear();
+            products.forEach(product => {
+                store.add(product);
+            });
+        })
+        .catch(error => console.error('Error fetching products:', error));
+}
