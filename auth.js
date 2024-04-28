8d0ddf78a67016c2d7f89a6fd91a4fe5c1f04159
satheesh kumar <4798470+dreamstudio-satheesh@users.noreply.github.com>
2024-04-28 19:32:18 +0530

    app

--- auth.js
+++ auth.js
@@ -1,12 +1,1 @@
-// Check if user is logged in
-function checkLogin() {
-    const token = localStorage.getItem('token');
-    if (!token) {
-        // If no token is found, redirect to the login page
-        window.location.href = '/index.html'; // Adjust the path as necessary
-    }
-    // You can add more checks here, like verifying the token with the server
-}
 
-// Run checkLogin when the document loads
-document.addEventListener('DOMContentLoaded', checkLogin);
