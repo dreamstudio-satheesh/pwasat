0d04b779cbb0036a3e335f042ecd8bee4264aabe
satheesh kumar <4798470+dreamstudio-satheesh@users.noreply.github.com>
2024-04-29 13:25:49 +0530

    app

--- auth.js
+++ auth.js
@@ -10,3 +10,40 @@ function checkLogin() {
 
 // Run checkLogin when the document loads
 document.addEventListener('DOMContentLoaded', checkLogin);
+
+
+
+function logout() {
+    // Clear user session/token from local storage or cookies
+    localStorage.removeItem('token'); // Assuming you store a token named 'token'
+    
+    clearIndexedDB();
+
+    // Redirect to login page or home page
+    window.location.href = 'login.html'; // Change '/login.html' to your login page URL
+}
+
+
+function clearIndexedDB() {
+    var dbRequest = indexedDB.open('satDB', 1);
+
+    dbRequest.onsuccess = function(event) {
+        var db = event.target.result;
+        var transaction = db.transaction(['categories', 'products'], 'readwrite');
+        transaction.objectStore('categories').clear();
+        transaction.objectStore('products').clear();
+        
+        transaction.oncomplete = function() {
+            console.log('All data cleared.');
+        };
+        
+        transaction.onerror = function(event) {
+            console.error('Error clearing data:', event.target.error);
+        };
+    };
+
+    dbRequest.onerror = function(event) {
+        console.error('Error accessing database:', event.target.error);
+    };
+}
+
